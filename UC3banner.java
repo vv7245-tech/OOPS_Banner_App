bb67f845745e35d3b8bd9a893f4f543eab6e1afd
vv7245-tech <vv7245@srmist.edu.in>
2026-02-17 09:04:45 +0530

    added UC3banner.java

---
+++ UC3banner.java
@@ -0,0 +1,14 @@
+public class UC3banner {
+    public static void main(String[] args) {
+
+        // Hint: Use String.join(" ", ...) to join the parts of each line with spaces.
+
+        System.out.println(String.join(" ", "  *** ", "   ***** "));
+        System.out.println(String.join(" ", " **  ** ", " **    "));
+        System.out.println(String.join(" ", " **  ** ", " **    "));
+        System.out.println(String.join(" ", " **  ** ", " ***** "));
+        System.out.println(String.join(" ", " **  ** ", "    ** "));
+        System.out.println(String.join(" ", " **  ** ", " **  ** "));
+        System.out.println(String.join(" ", "  *** ", "   ***** "));
+    }
+}
